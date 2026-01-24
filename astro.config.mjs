0cd87bbaae434626ebbd9616ba1e07d970be962b
zeduchen <zeduchen@gmail.com>
2026-01-25 01:59:19 +0800

    seo optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,23 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
+const isBuild = process.argv.includes('build');
+
 export default defineConfig({
     site: 'https://www.md2text.com',
     output: 'server',
     adapter: cloudflare({
         imageService: 'compile',
     }),
-    integrations: [react(), tailwind()],
+    integrations: [react(), tailwind(), sitemap()],
     vite: {
         resolve: {
-            alias: {
+            alias: isBuild ? {
                 "react-dom/server": "react-dom/server.edge",
-            },
+            } : undefined,
         },
     },
 });
