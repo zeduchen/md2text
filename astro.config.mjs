0b2e4d1410e37ba7dbf86217b78c440520a5f6db
zeduchen <zeduchen@gmail.com>
2026-01-25 00:59:21 +0800

    fix deploy bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: cloudflare(),
+    adapter: cloudflare({
+        imageService: 'compile',
+    }),
     integrations: [react(), tailwind()]
 });
