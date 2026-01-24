572515c3a88cbb388f30f32d445104a10967c588
zeduchen <zeduchen@gmail.com>
2026-01-25 00:55:28 +0800

    use cloudflare worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: cloudflare(),
     integrations: [react(), tailwind()]
 });
